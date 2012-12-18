7185b130514147a6cfb7676f3fc7e85d9625076a
IgnAnna <ametist_21@mail.ru>
2012-12-18 12:26:09 +0600

    SelectionMode-MultiSimpel

--- Redactor_IP/Form1.Designer.cs
+++ Redactor_IP/Form1.Designer.cs
@@ -151,6 +151,7 @@ namespace Redactor_IP
             this.ShapesList.FormattingEnabled = true;
             this.ShapesList.Location = new System.Drawing.Point(12, 27);
             this.ShapesList.Name = "ShapesList";
+            this.ShapesList.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
             this.ShapesList.Size = new System.Drawing.Size(175, 251);
             this.ShapesList.TabIndex = 3;
             this.ShapesList.SelectedIndexChanged += new System.EventHandler(this.ShapesList_SelectedIndexChanged);
